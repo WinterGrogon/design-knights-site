a3cbeaee621ee9dcf7a0d19acc09755a1738ea8b
WinterGrogon <galife805@gmail.com>
2025-12-06 13:32:11 +0300

    Сделали секцию с проектами, сделали открытие слайдов

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,59 @@
-document.querySelectorAll('.numeric-section > .container').forEach((container, index) => {
-    const sectionName = document.createElement('div');
-    sectionName.className = 'section-name';
-    container.prepend(sectionName);
+const slidesImages = document.querySelectorAll('.projects__slide img');
+const slidesRadio = document.querySelectorAll('.projects__switchers input');
+
+
+// Функция обновления кликабельности изображения
+// срабатывает только на активных слайдах
+function updateActiveSlide() {
+    slidesImages.forEach(el => {
+        el.style.pointerEvents = 'none';
+    });
+
+    const checked = document.querySelector('.projects__switchers input:checked');
+
+    if (checked) {
+        const index = Array.from(slidesRadio).indexOf(checked);
+        const activeImg = slidesImages[index];
+        if (activeImg) activeImg.style.pointerEvents = 'auto';
+    }
+}
+
+
+// Обновляем при смене слайда
+slidesRadio.forEach(radio => {
+    radio.addEventListener('change', updateActiveSlide);
+});
+
+
+// Вызываем инициализации
+updateActiveSlide();
+
+
+// Вешаем событие клика на изображения внутри слайдера
+slidesImages.forEach((img, i) => {
+    img.addEventListener('click', (e) => {
+        e.stopPropagation();
+
+        const modal = document.createElement('div');
+        modal.className = 'modal';
+        modal.innerHTML = `
+            <div class="modal__content">
+                <img src="${img.src}" alt="${img.alt}">
+            </div>
+        `;
+        document.body.appendChild(modal);
+
+        // Добавляем класс после создания элемента
+        requestAnimationFrame(() => {
+            modal.classList.add('active');
+        });
+
+        // Закрытие
+        modal.addEventListener('click', () => {
+            modal.classList.remove('active');
+            setTimeout(() => {
+                modal.remove();
+            }, 500);
+        });
+    });
 });
